1c675326e7bbc9fe85122baa7d36bace72411c61
Dragos0403 <109337697+Dragos0403@users.noreply.github.com>
2024-06-26 10:23:13 +0300

    Rated functional

--- React Movies/src/pages/home/mutation.tsx
+++ React Movies/src/pages/home/mutation.tsx
@@ -1,18 +1,20 @@
-export const rateMovie = async (movieId: number, rating :number) => {
-    // Facem o solicitare HTTP pentru a obține un nou `guest_session_id`
-    const res = await fetch(`"https://api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem("guest_session_id")}?api_key=02f3192413ff2d7b050242e52d7c6741`, 
-    {
+export const rateMovie = async (movieId: number, rating: number) => {
+    // Construim URL-ul pentru solicitarea HTTP pentru a acorda un rating unui film
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem("guest_session_id")}&api_key=02f3192413ff2d7b050242e52d7c6741`;
+
+    // Facem o solicitare POST către API-ul TMDB
+    const res = await fetch(url, {
         method: "POST",
         headers: {
-            accept: 'application/json',  // Specificăm că acceptăm răspunsuri JSON
-            "content-type": "aplication/json;charset=utf-8",
+            accept: 'application/json', 
+            "Content-Type": "application/json;charset=utf-8", 
         },
-        body:`{"value": ${rating}}`,
+        body: JSON.stringify({ value: rating }), 
     });
 
-    // Verificăm dacă răspunsul este OK
+  
     if (!res.ok) {
-        // Aruncăm o eroare dacă răspunsul nu este OK
+       
         throw new Error("Network response was not ok");
     }
 
@@ -21,25 +23,27 @@ export const rateMovie = async (movieId: number, rating :number) => {
     return data;
 };
 
-export const rateTvShow = async (tvShowId: number, rating :number) => {
-    // Facem o solicitare HTTP pentru a obține un nou `guest_session_id`
-    const res = await fetch(`"https://api.themoviedb.org/3/tv/${tvShowId}/rating?guest_session_id=${localStorage.getItem("guest_session_id")}?api_key=02f3192413ff2d7b050242e52d7c6741`, 
-    {
+export const rateTvShow = async (tvShowId: number, rating: number) => {
+   
+    const url = `https://api.themoviedb.org/3/tv/${tvShowId}/rating?guest_session_id=${localStorage.getItem("guest_session_id")}&api_key=02f3192413ff2d7b050242e52d7c6741`;
+
+    
+    const res = await fetch(url, {
         method: "POST",
         headers: {
-            accept: 'application/json',  // Specificăm că acceptăm răspunsuri JSON
-            "content-type": "aplication/json;charset=utf-8",
+            accept: 'application/json',
+            "Content-Type": "application/json;charset=utf-8", 
         },
-        body:`{"value": ${rating}}`,
+        body: JSON.stringify({ value: rating }), 
     });
 
-    // Verificăm dacă răspunsul este OK
+
     if (!res.ok) {
-        // Aruncăm o eroare dacă răspunsul nu este OK
+       
         throw new Error("Network response was not ok");
     }
 
-    // Parsăm răspunsul JSON și îl returnăm
+
     const data = await res.json();
     return data;
 };
